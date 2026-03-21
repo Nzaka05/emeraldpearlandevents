813bb2cf4afb61025381ed87d2b96acde8dd3b71
Nzaka05 <nzakagideon05@gmail.com>
2026-03-21 09:58:13 +0300

    fix: add validateParam to validateObjectId util, fix CloudinaryStorage import

--- staff-system/utils/validateObjectId.js
+++ staff-system/utils/validateObjectId.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
+
 function isValidObjectId(id) {
     return mongoose.Types.ObjectId.isValid(id);
 }
-module.exports = { isValidObjectId };
+
+function validateParam(paramName) {
+    return (req, res, next) => {
+        const id = req.params[paramName];
+        if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ success: false, error: `Invalid ${paramName}` });
+        }
+        next();
+    };
+}
+
+module.exports = { isValidObjectId, validateParam };
